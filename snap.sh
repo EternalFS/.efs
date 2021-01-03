8ef81c032c10bae2dcca3306e327ea79c6caa70b
DarkStar <darkstar@unisocial.net>
2021-01-03 06:30:08 -0600

    1st

---
+++ snap.sh
@@ -0,0 +1,25 @@
+. this
+
+backupTo="$this/.efs/backup"
+
+pre(){
+mkdir -p "$backupTo"
+}
+
+getDatetime(){
+date=`date`
+echo $date
+}
+
+snapAllVols(){
+cd $this
+btrfs sub create "$backupTo/$date"
+btrfs sub snap -r my-eternal/Omni "$backupTo/$date"/
+btrfs sub snap -r my-eternal/Recovery-img "$backupTo/$date"/
+btrfs sub snap -r my-eternal/Stor "$backupTo/$date"/
+btrfs sub snap -r my-eternal/Uni "$backupTo/$date"/
+}
+
+pre
+getDatetime
+snapAllVols
