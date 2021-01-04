8f52e3c7a8c86a5ee544d60a41c15c54bd1e54d6
DarkStar <darkstar@unisocial.net>
2021-01-04 03:31:45 -0600

    2nd

--- snap.sh
+++ snap.sh
@@ -15,7 +15,6 @@ snapAllVols(){
 cd $this
 btrfs sub create "$backupTo/$date"
 btrfs sub snap -r my-eternal/Omni "$backupTo/$date"/
-btrfs sub snap -r my-eternal/Recovery-img "$backupTo/$date"/
 btrfs sub snap -r my-eternal/Stor "$backupTo/$date"/
 btrfs sub snap -r my-eternal/Uni "$backupTo/$date"/
 }
